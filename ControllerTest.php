fa51b5cccda97995bf75e9fc50871c6d2c3a7703
Moreno-florian <62134463+Moreno-florian@users.noreply.github.com>
2020-09-16 17:12:50 +0200

    Add files via upload

--- ControllerTest.php
+++ ControllerTest.php
@@ -1,11 +1,24 @@
 <?php
 require_once '../Model/Dao/DBconnection.php';
+require_once '../Model/Dao/DaoWeight.php';
+require_once '../Model/ClassWeight.php';
+/*********************************************************************************************************************************************************/
+require_once '../Model/Dao/DBconnection.php';
 require_once '../Model/Dao/DaoDate.php';
 require_once '../Model/ClassDate.php';
-require_once '../View/Consultation.php';
+/*********************************************************************************************************************************************************/
+switch (key($_GET)) {
+    case 'ControllerTest':
+        $list = (new DaoDate())->showDate();
+        include_once "../View/Ajout.phtml";
+        break;
 
+    case 'brique':
+        if ($_POST['firstname'] == 2) {
+            echo ("Firstname=2");
+        } else {
+            echo ("Ok, on est dans la branche de firstname =2");
+        }
 
-
-$idSnake = intval($_POST['firstname']);       // Transform id value from string to integer
-(new DaoDate())->deleteDate($idSnake); 
-header('Location: ../View/Consultation.php');
+        break;
+}
